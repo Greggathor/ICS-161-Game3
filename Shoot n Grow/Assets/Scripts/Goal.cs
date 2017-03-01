ed349cd466ca5a981c52f4091c17093b70da8b9f
David Liu <xdliuu95@hotmail.com>
2017-03-01 02:21:46 -0800

    Level 1 Roughly Done
    - Level 1's layout is done (for now)
    - Signs on how to expand is done
    - Temporary switches has been made for player 2 to activate
    - A "goal flag" has  been added and level is complete once both players
    touches it
    - Goal flag doesnt necessarily advance to next level right now, but can
    be used to check if level is completed

---
+++ Shoot n Grow/Assets/Scripts/Goal.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+    public bool player1done;
+    public bool player2done;
+    public bool leveldone;
+
+	// Use this for initialization
+	void Start () {
+
+    player1done = false;
+    player2done = false;
+    leveldone = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    if(player1done && player2done)
+    {
+        leveldone = true;
+    }
+
+    if(leveldone)
+    {
+    //Do stuff to advance
+    }
+
+    //End of Update	
+	}
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.name == "Player1"){
+            player1done = true;
+        }
+        if(other.gameObject.name == "Player2"){
+            player2done = true;
+        }
+    }
+
+}
+
+
