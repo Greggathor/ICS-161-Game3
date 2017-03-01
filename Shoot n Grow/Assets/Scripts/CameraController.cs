cffb5f0bc7c84a864687381c6652af3f8bd1419d
Gregg Sueda <gsuedajr@gmail.com>
2017-03-01 00:12:30 -0800

    Starting Level 1
    -Added signposts
    -Started level 1 design

---
+++ Shoot n Grow/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player1;
+
+	private Vector3 offset;
+
+	void Start(){
+		offset = transform.position - player1.transform.position;
+	}
+
+	void LateUpdate(){
+		transform.position = player1.transform.position + offset;
+	}
+}
---
+++ Shoot n Grow/Assets/Scripts/DisplayInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayInfo : MonoBehaviour {
+
+	public GameObject panel;
+	public GameObject text;
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Player")){
+			panel.SetActive(true);
+			text.SetActive(true);
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.gameObject.CompareTag("Player")){
+			panel.SetActive(false);
+			text.SetActive(false);
+		}
+	}
+}
