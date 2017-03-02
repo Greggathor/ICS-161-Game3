ca232f9db4aaa86f9de4cafeacdf6b642a223a88
David Liu <xdliuu95@hotmail.com>
2017-03-01 16:10:46 -0800

    Level 2 Roughly Done

---
+++ Shoot n Grow/Assets/Scripts/BreakableObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableObject : MonoBehaviour {
+
+    public int hitcount;
+
+    // Use this for initialization
+    void Start () {
+
+    hitcount = 0;
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+
+    if(hitcount >= 1)
+    {
+        Destroy(gameObject);
+    }
+
+    //End of Update 
+    }
+
+    void OnCollisionEnter(Collision other){
+        if(other.gameObject.CompareTag("Shot")){
+            hitcount++;
+        }
+    }
+}
